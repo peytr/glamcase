7152518866693e4e79ca2e1b556e9fa2971bc499
Cameo <hello@aeglir.com.au>
2018-06-13 08:04:19 +1000

    Resolve merge conflicts

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const caseRouter = require('./cases/caseRouter')
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.redirect('/cases')
+})
+
+app.use('/cases', caseRouter)
+
+app.listen(3000, () => {
+    console.log("Express server listening on port 3000 :D");
+});
+
 const url = '../example-case.json'
 
 
@@ -23,20 +41,3 @@ function showCovers(res) {
     })
     
 }
-const express = require('express');
-const bodyParser = require('body-parser')
-const caseRouter = require('./cases/caseRouter')
-
-const app = express();
-
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-    res.redirect('/cases')
-})
-
-app.use('/cases', caseRouter)
-
-app.listen(3000, () => {
-    console.log("Express server listening on port 3000 :D");
-});
